9c13b855b09744b23361c06086e31f053d5e38ad
adrsch <35346279+adrsch@users.noreply.github.com>
2020-08-24 21:58:58 -0700

    Ignore unneeded files

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,5 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
   },
-  ignorePatterns: ['dist/**'],
+  ignorePatterns: ['lib/**'],
 };
